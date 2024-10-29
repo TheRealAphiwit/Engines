b4592c04c0158e1aa4f06b1c66fd644373b5329a
TheRealAphiwit <144233520+TheRealAphiwit@users.noreply.github.com>
2024-10-29 14:58:30 +0100

    Lesson 1

---
+++ HopefullyWorkEngine/HopefullyWorkEngine.cpp
@@ -0,0 +1,52 @@
+// HopefullyWorkEngine.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <glfw3.h>
+
+int main()
+{
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW\n";
+        return -1;
+    }
+    GLFWwindow* NewWindow;
+    NewWindow = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    glfwMakeContextCurrent(NewWindow);
+    if (!NewWindow)
+    {
+        std::cout << "Failed to create window\n";
+        glfwTerminate();
+        return -1;
+    }
+    while (!glfwWindowShouldClose(NewWindow))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glClearColor(1.0f, 0.5f, 0.f, 1.f);
+
+        glBegin(GL_TRIANGLES);
+
+        glVertex2f(0.5f, 0.0f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(-0.5f, 0.5);
+
+        glEnd();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
