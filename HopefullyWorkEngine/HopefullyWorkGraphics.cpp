fae08487ae89e066a6691fd4ef2ad191b3584a42
TheRealAphiwit <aphiwit.lekphet@edu.futuregames.nu>
2024-10-29 15:38:58 +0100

    Started Encapsulation
    Still in the early slides

---
+++ HopefullyWorkEngine/HopefullyWorkGraphics.cpp
@@ -0,0 +1,8 @@
+#include "HopefullyWorkGraphics.h"
+#include <iostream>
+// #include <glad/glad.h>
+#include <glfw3.h>
+#include <direct.h>
+// #include "Shader.h"
+#include <Windows.h>
+#include <stdio.h>
