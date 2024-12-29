22150f6191da84b78ff35dff646c275664c5af33
TheRealAphiwit <aphiwit.lekphet@edu.futuregames.nu>
2024-12-29 14:46:47 +0100

    Saved before big change

--- Mahjong Engine/Mahjong Engine/Dots/EntityHandler.h
+++ Mahjong Engine/Mahjong Engine/Dots/EntityHandler.h
@@ -1,2 +1,34 @@
 #pragma once
 #include <vector>
+#include <memory>
+#include <string>
+
+// Needed components for entity handling
+class VirtualObject;
+class Mesh;
+class Texture;
+class Shader;
+
+namespace DotsRendering
+{
+	class EntityHandler
+	{
+	public:
+		// Singelton
+		static EntityHandler& GetInstance()
+		{
+			static EntityHandler instance;
+			return instance;
+		}
+
+		// Prevent copy and overwriting
+		EntityHandler(const EntityHandler&) = delete;
+		EntityHandler& operator=(const EntityHandler&) = delete;
+
+	private:
+		EntityHandler();
+		~EntityHandler();
+
+		std::vector<VirtualObject*> myObjects;
+	};
+}
