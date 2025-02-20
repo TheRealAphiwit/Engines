f1d71f3db4ad5f23607a5edc3b39b9747e9c1ffb
TheRealAphiwit <aphiwit.lekphet@edu.futuregames.nu>
2025-02-20 14:59:01 +0100

    More physics engine methods

--- Mahjong Engine/Mahjong Engine/Winds/Winds_Physics.cpp
+++ Mahjong Engine/Mahjong Engine/Winds/Winds_Physics.cpp
@@ -1,5 +1,114 @@
 #include "Winds_Physics.h"
+#include "../Mahjong Engine/DragonEngine.h"
+// #include "GameObject.h"
+#include "Collisions.h"
+// #include "Intersections.h"
+#include <iostream>
+#include "../Dots/VirtualObject.h"
+#include <gtc/matrix_transform.hpp>
+// #include "MonkeyMath.h"
+// #include "Raycast.h"
+#include <gtx/string_cast.hpp>
 
-Winds::Winds_Physics::Winds_Physics()
+namespace Winds
 {
+	Winds_Physics::Winds_Physics(Engine::DragonEngine* aEngine)
+	{
+		myEngine = aEngine;
+
+		main_plane = new PlaneCollider(glm::vec3(0, 1, 0), 1);
+		main_plane->position = glm::vec3(0, 0, 0);
+	}
+
+	void Winds_Physics::Simulate(const float aDeltaTime)
+	{
+		float limitDt = glm::min(aDeltaTime, 0.01f);
+
+		//colliders = UpdatePhysicsScene();
+		//std::vector<Collision> collisions = CheckIntersections(colliders);
+
+		//ApplyGravity(colliders, limitDt);
+
+		//HandleCollisions(collisions);
+
+		//ApplyVelocity(colliders, limitDt);
+
+		UpdateVisuals();
+	}
+
+	void Winds_Physics::ApplyGravity(std::vector<Collider*> colliders, const float& dt)
+	{
+		for (Collider* c : colliders)
+		{
+			if (!c->isKinematic && c->hasGravity)
+			{
+				c->velocity += glm::vec3(0, GravityMultiplier, 0) * dt;
+			}
+		}
+	}
+
+	void Winds_Physics::ApplyVelocity(std::vector<Collider*> colliders, const float& dt)
+	{
+		for (Collider* c : colliders)
+		{
+			if (!c->isKinematic)
+			{
+				std::cout << glm::to_string(c->angularVelocity) << std::endl;
+				c->position += c->velocity * dt;
+				c->transform[3] = glm::vec4(c->position, 1.0f);
+
+				if (glm::length(c->angularVelocity) > 0.0001f)
+				{
+					glm::vec3 angularVelocityNorm = glm::normalize(c->angularVelocity);
+					glm::quat angularRotation = glm::angleAxis(glm::length(c->angularVelocity) * dt, angularVelocityNorm);
+					glm::mat3 rotationDelta = glm::mat3_cast(angularRotation);
+					c->transform = glm::mat4(rotationDelta) * c->transform;
+				}
+
+				if (c->mass > 0)
+				{
+					c->velocity *= glm::pow(1.0f - LinearDrag, dt);
+				}
+				if (c->mass > 0)
+				{
+					c->angularVelocity *= glm::exp(-AngularDrag * dt);
+				}
+
+				glm::mat3 rotationMatrix = glm::mat3(c->transform);
+				glm::mat3 inertiaTensorInWorldSpace = rotationMatrix * c->momentOfInertia * glm::transpose(rotationMatrix);
+				c->inverseMomentOfInertia = glm::inverse(inertiaTensorInWorldSpace);
+			}
+			else
+			{
+				c->velocity = glm::vec3(0, 0, 0);
+				c->angularVelocity = glm::vec3(0, 0, 0);
+			}
+		}
+	}
+
+	void Winds_Physics::HandleCollisions(std::vector<Collision> collisions)
+	{
+		std::vector<Collision> dynamicDynamicCollisions;
+		std::vector<Collision> staticDynamicCollisions;
+
+		for (Collision c : collisions)
+		{
+			bool A_isDynamic = !c.col1->isKinematic;
+			bool B_isDynamic = !c.col2->isKinematic;
+
+			if (A_isDynamic && B_isDynamic)
+			{
+				dynamicDynamicCollisions.push_back(c);
+			}
+			else
+			{
+				staticDynamicCollisions.push_back(c);
+			}
+		}
+
+		HandleDynamicDynamic(dynamicDynamicCollisions);
+		HandleStaticDynamic(staticDynamicCollisions);
+	}
+
+
 }
--- Mahjong Engine/Mahjong Engine/Winds/Winds_Physics.h
+++ Mahjong Engine/Mahjong Engine/Winds/Winds_Physics.h
@@ -3,10 +3,30 @@
 #include <glm.hpp>
 #include <vector>
 #include <optional>
+//#include "Raycast.h"
+
+const float GravityMultiplier = -9.82f;
+const float Restitution = 0.2f;
+const float LinearDrag = 0.32f;
+const float AngularDrag = 0.000005f;
 
 namespace Engine
 {
+	class Input;
 	class DragonEngine;
+}
+
+namespace DotsRendering
+{
+	class Camera;
+	class OpenGLFrameBuffer; // Empty frwd dec
+}
+
+namespace Winds
+{
+	class Collider;
+	class PlaneCollider;
+	struct Collision;
 }
 
 namespace Winds
@@ -14,19 +34,26 @@ namespace Winds
 	class Winds_Physics
 	{
 	public:
-		Winds_Physics();
-		void Simulate(const float deltaTime);
+		Winds_Physics(Engine::DragonEngine* aEngine);
+		void Simulate(const float aDeltaTime);
 
-		void ApplyGravity();
-		void ApplyVelocity();
+		void ApplyGravity(std::vector<Collider*> colliders, const float& dt);
+		void ApplyVelocity(std::vector<Collider*> colliders, const float& dt);
 
-		void HandleCollisions();
-		void HandleStaticDynamic();
-		void HandleDynamicDynamic();
+		void HandleCollisions(std::vector<Collision> collisions);
+		void HandleStaticDynamic(std::vector<Collision> collisions);
+		void HandleDynamicDynamic(std::vector<Collision> collisions);
 
+		std::vector<Collider*> UpdatePhysicsScene();
+		std::vector<Collision> CheckIntersections(std::vector<Collider*> colliders);
 		void UpdateVisuals();
 
+		// bool MonkeyRaycast(const Ray& aRay, RayHit& aHit);
+
 	private:
+		std::vector<Collider*> colliders;
 		Engine::DragonEngine* myEngine;
+
+		PlaneCollider* main_plane;
 	};
 }
